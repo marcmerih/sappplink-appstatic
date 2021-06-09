6277770c8fe935f0fe05a71d4474ce89119bb230
Marc <merih.atasoy@gmail.com>
2021-06-08 21:05:45 -0400

    added firebase

--- appstatic/src/environments/environment.ts
+++ appstatic/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBFcyrkuOXkjDympqoXwWYZYCwxoBNV6uk",
+    authDomain: "sappplink.firebaseapp.com",
+    projectId: "sappplink",
+    storageBucket: "sappplink.appspot.com",
+    messagingSenderId: "119570821835",
+    appId: "1:119570821835:web:ca616718ac89d682d6614e",
+    measurementId: "G-CDXNTYFK16"
+  }
 };
 
 /*
