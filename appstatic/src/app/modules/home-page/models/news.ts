907a809e523262b70c87bf3d85cff13735ba20d9
Marc <merih.atasoy@gmail.com>
2021-06-29 21:55:54 -0400

    more commits

--- appstatic/src/app/modules/home-page/models/news.ts
+++ appstatic/src/app/modules/home-page/models/news.ts
@@ -1,7 +1,10 @@
 export interface NewsItem {
     backgroundImg: string;
+    pageImg?: string;
     title: string;
     isNews: boolean;
     date: string | null;
     numberOfViews: number;
+    content?: string;
+    author?: string;
 }
